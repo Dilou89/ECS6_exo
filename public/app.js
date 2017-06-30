2b5ffc230cfcbf1367342c2ba9529c2c2cfedbf7
Dilou89 <odil_cadiou@orange.fr>
2017-06-30 11:47:49 +0200

    partie 3 finie

--- public/app.js
+++ public/app.js
@@ -1,22 +1,42 @@
 
+let tab = [];
+let indices = [];
 document.getElementById('valid').addEventListener('click', e => {
 	let prenom = document.getElementById('firstName').value;
-	const maj = prenom.charAt(0).toUpperCase() + prenom.substring(1).toLowerCase();
+	let maj = prenom.charAt(0).toUpperCase() + prenom.substring(1).toLowerCase();
+
 
 	if (maj == "") {
 		document.getElementById("reponse").innerHTML = `Bonjour Père Noel!`;
 	} else {
 		document.getElementById("reponse").innerHTML = `Bonjour ${maj}!`;
 	}
+
+	tab.push(prenom);
+
 	let list = document.getElementById("tableau").rows;
 	let newList = document.getElementById("tableau").insertRow(-1);
 	let textnode = document.createTextNode(` ${maj}`);
 	newList.appendChild(textnode);
 
-	document.getElementById('search').addEventListener('click', e => {
-		let saisie = document.getElementById('abc').value;
+//Ajouter un champ de recherche et un bouton au dessus de l'historique. 
+// Lorsque ce bouton est cliqué, n'afficher que les noms commençant 
+// par la chaîne de caractères saisie dans ce champ ('case insensitive')
 
-		let debut = maj.substr(0, 3);
-		console.log(debut);
-	});
+document.getElementById('search').addEventListener('click', e => {
+	let saisie = document.getElementById('abc').value;
+	let majS = saisie.charAt(0).toUpperCase() + saisie.substring(1).toLowerCase();
+	let debut = maj.substr(0, 3);
+
+	let filt = debut.includes(majS);
+	if(filt==true){
+
+		indices.push(prenom);
+	}
+	document.getElementById("tri").innerHTML=`${indices}`;
 });
+});
+
+
+
+
